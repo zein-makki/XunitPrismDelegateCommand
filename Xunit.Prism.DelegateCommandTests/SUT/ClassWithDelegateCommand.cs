96bd8ba799b8de38c76f00c1ec2234069c120464
Zein Makki <zeinsmakki@gmail.com>
2018-11-13 20:02:41 +0200

    Intial Commit

---
+++ Xunit.Prism.DelegateCommandTests/SUT/ClassWithDelegateCommand.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Practices.Prism.Commands;
+using System;
+using System.Threading;
+
+namespace Xunit.Prism.DelegateCommandTests
+{
+    public class ClassWithDelegateCommand
+    {
+        public DelegateCommand delegateCommand1;
+
+        public ClassWithDelegateCommand()
+        {
+            delegateCommand1 = new DelegateCommand(() => { }, () => true);
+        }
+
+        public void RaiseCanExecuteChanged(int commandNumber)
+        {
+            SomeLongRunningCode();
+
+            delegateCommand1.RaiseCanExecuteChanged();
+        }
+
+        private void SomeLongRunningCode()
+        {
+            //Thread.Sleep(TimeSpan.FromSeconds(new Random().Next(4, 6)));
+            for (int i = 0, j = 0; i < int.MaxValue; i++)
+                j++;
+        }
+    }
+}
---
+++ Xunit.Prism.DelegateCommandTests/Tests/DelegateCommandTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Xunit.Prism.DelegateCommandTests
+{
+    public class DelegateCommandTests
+    {
+        ClassWithDelegateCommand sut;
+        bool delegateCommand1CanExecuteChangedFired = false;
+
+        public DelegateCommandTests()
+        {
+            sut = new ClassWithDelegateCommand();
+
+            sut.delegateCommand1.CanExecuteChanged += DelegateCommand1_CanExecuteChanged;
+        }
+
+        [Fact]
+        public void DelegateCommand1Test()
+        {
+            sut.RaiseCanExecuteChanged(1);
+
+            Assert.True(delegateCommand1CanExecuteChangedFired);
+        }
+
+        private void DelegateCommand1_CanExecuteChanged(object sender, EventArgs e)
+        {
+            delegateCommand1CanExecuteChangedFired = true;
+        }
+    }
+    // Generate more of the same test class to reproduce the issue by running 20 similar tests
+
+    public class DelegateCommandTests02 : DelegateCommandTests { }
+    public class DelegateCommandTests03 : DelegateCommandTests { }
+    public class DelegateCommandTests04 : DelegateCommandTests { }
+    public class DelegateCommandTests05 : DelegateCommandTests { }
+    public class DelegateCommandTests06 : DelegateCommandTests { }
+    public class DelegateCommandTests07 : DelegateCommandTests { }
+    public class DelegateCommandTests08 : DelegateCommandTests { }
+    public class DelegateCommandTests09 : DelegateCommandTests { }
+    public class DelegateCommandTests10 : DelegateCommandTests { }
+    public class DelegateCommandTests11 : DelegateCommandTests { }
+    public class DelegateCommandTests12 : DelegateCommandTests { }
+    public class DelegateCommandTests13 : DelegateCommandTests { }
+    public class DelegateCommandTests14 : DelegateCommandTests { }
+    public class DelegateCommandTests15 : DelegateCommandTests { }
+    public class DelegateCommandTests16 : DelegateCommandTests { }
+    public class DelegateCommandTests17 : DelegateCommandTests { }
+    public class DelegateCommandTests18 : DelegateCommandTests { }
+    public class DelegateCommandTests19 : DelegateCommandTests { }
+    public class DelegateCommandTests20 : DelegateCommandTests { }
+}
